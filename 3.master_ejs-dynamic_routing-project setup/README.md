119154a0e5226e09983a0ed2c07c11cca0ff5c3d
Kunal11102002 <bhardwajkunal0111@gmail.com>
2026-07-19 01:11:36 +0530

    commit

--- 3.master_ejs-dynamic_routing-project setup/README.md
+++ 3.master_ejs-dynamic_routing-project setup/README.md
@@ -13,3 +13,8 @@ setting up public static flies : mtlv hum use krna chahte ha ccs vanilla js imag
 dynamic routing:
        dynamic routing
        how to get data coming from frontend at backend route
+
+how tp create dynamic routing:
+sbse pehle brwoser pe jaao or url likho jo apko chahiye and enter it (eg: localost:3000/profile/kunal)ab error ayega not found 
+so create this url
+ab us url ko agr dynamic banana h to realize kro konsa part dynamic ha aur us aprt aage route me : laga do
--- 3.master_ejs-dynamic_routing-project setup/index.js
+++ 3.master_ejs-dynamic_routing-project setup/index.js
@@ -15,6 +15,19 @@ app.get('/', (req, res) => {
     // res.send("chal raha ha") isko ej k liye comment out kra
     res.render("index"); //render vahi hoga jo views file me h . so views file me index.ejs h so hum index hi likhte hai ,pura extension nahi 
 });
+//dynamic route
+app.get('/profile/:username', (req, res) => {
+
+    // res.send("hello bhai kya scn")
+    res.send(req.params.username)
+    // jaise hum localhost:3000/profile/kunal pe h to.. frontend se kunal aya backend me
+    //  or save hua  username me. or backend se humne phir response bhja through the above line
+    // phir se kunal or frontend pe aya kunal....   route me /ritka h to ritika show hoga
+    // /sam h to sam show hoga......jo bhi username ki value h..
+
+    // yahan me :kunal liku :ritika likhu ye sbke liye chalega ...dynamic ban gyaab ye route
+
+});
 
 app.listen(3000, function () {
     console.log("its running")
