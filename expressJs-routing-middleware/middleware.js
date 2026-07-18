0066420066d67f5dc974a37ed10ad4941b6147e2
Kunal11102002 <bhardwajkunal0111@gmail.com>
2026-07-18 13:53:29 +0530

    commit

--- expressJs-routing-middleware/middleware.js
+++ expressJs-routing-middleware/middleware.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const app = express();
 
-const myLogger = function (req, res, next) {
+app.use(function (req, res, next) {
   console.log('LOGGED');
   next();
-};
-
-app.use(myLogger);
+});
+app.use(function (req, res, next) {
+  console.log('ek barr or');
+  next();
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+app.get('/about', (req, res) => {
+  res.send('Hello kya scn ha!');
 });
 
 app.listen(3000);
