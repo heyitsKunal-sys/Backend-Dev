65030e6a85edce328da9dd142fe6a050c0cf7894
Kunal11102002 <bhardwajkunal0111@gmail.com>
2026-07-27 00:27:50 +0530

    commit

--- 5.crud_with_ejs_and_serverSideRendering/app.js
+++ 5.crud_with_ejs_and_serverSideRendering/app.js
@@ -1,17 +1,28 @@
 const express = require('express');
 const app = express();
 const path = require('path')
-
+const userModel = require('./models/user')
 app.set("view engine", 'ejs')
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.render('index');
 })
-app.get('/read',(req,res)=>{
+app.get('/read', (req, res) => {
     res.render('read');
+})
+app.post('/create', async (req, res) => {
+    let { name, email, image } = req.body;
+    let user = await userModel.create({
+        name,                                      //name: name ko sidha name likh diya kyuki dono side name name tha email:email ....
+        email,
+        image
+    })
+
+    res.send(createdUser)
+
 })
 
 app.listen(3000);
