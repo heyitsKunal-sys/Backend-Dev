5d87e986742f6a97294e2d4f58374170d97ab52a
Kunal11102002 <bhardwajkunal0111@gmail.com>
2026-07-27 12:35:05 +0530

    commit

--- 5.crud_with_ejs_and_serverSideRendering/app.js
+++ 5.crud_with_ejs_and_serverSideRendering/app.js
@@ -13,17 +13,26 @@ app.get('/', (req, res) => {
 
     res.render('index');
 })
+
 app.get('/read', async (req, res) => {
     let users = await userModel.find()
-    console.log(users)
-    console.log(users.length)
-    res.render('read', {users});
+
+    res.render('read', { users });
+})
+app.get('/edit/:userid', async (req, res) => {
+    let user = await userModel.findOne({ _id: req.params.userid })
+    res.render('edit',{user})
+})
+app.post('/update/:userid', async (req, res) => {
+    let {image, name , email} = req.body
+    let user = await userModel.findOneAndUpdate({ _id: req.params.userid },{image, name , email}, {new: true})
+    res.redirect('/read')
 })
 
 
 app.get('/delete/:id', async (req, res) => {
-    let users = await userModel.findOneAndDelete({_id: req.params.id})
-    
+    let users = await userModel.findOneAndDelete({ _id: req.params.id })
+
     res.redirect('/read');
 })
 
