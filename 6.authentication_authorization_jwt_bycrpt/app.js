97644abbb46e66ef738d26f9a47a09a19cf90425
Kunal11102002 <bhardwajkunal0111@gmail.com>
2026-07-28 13:21:29 +0530

    commit

--- 6.authentication_authorization_jwt_bycrpt/app.js
+++ 6.authentication_authorization_jwt_bycrpt/app.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const app = express()
+const cookieParser = require('cookie-parser')
+const bcrypt = require('bcrypt')
 
-app.get('/',(req, res)=>{
-    res.cookie('name', 'kunal')
-    res.send( 'done')
+
+app.use(cookieParser())
+
+
+app.get('/', (req, res) => {
+    res.cookie('name', 'kunal')    //set krne ke liye res use hota ha(yahan humne cookie set kri)
+    res.send('done')
+
+})
+
+app.get('/read', (req, res) => {
+    console.log(req.cookies)    // read krne k liye req use hota hai(yahan humne cokkie read kri)
+    res.send('read page')
 
 })
 
