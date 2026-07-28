39d4863ddc5ef3d412711fa5ebd80d535a070bd5
Kunal11102002 <bhardwajkunal0111@gmail.com>
2026-07-28 13:33:32 +0530

    commit

--- 6.authentication_authorization_jwt_bycrpt/app.js
+++ 6.authentication_authorization_jwt_bycrpt/app.js
@@ -7,16 +7,34 @@ const bcrypt = require('bcrypt')
 app.use(cookieParser())
 
 
-app.get('/', (req, res) => {
-    res.cookie('name', 'kunal')    //set krne ke liye res use hota ha(yahan humne cookie set kri)
-    res.send('done')
+// app.get('/', (req, res) => {
+//     res.cookie('name', 'kunal')    //set krne ke liye res use hota ha(yahan humne cookie set kri)
+//     res.send('done')
 
+// })
+
+// app.get('/read', (req, res) => {
+//     console.log(req.cookies)    // read krne k liye req use hota hai(yahan humne cokkie read kri)
+//     res.send('read page')
+
+// })
+
+
+app.get('/',function(req,res){
+    bcrypt.genSalt(10 , function(err,salt){                  //salt ek random string hoti ha
+        bcrypt.hash('lovelylovely',salt, function(err, hash){
+            console.log(hash)
+            //$2b$10$zqFJAVsKwn/aEdafUiOb0OQ4qIT2oUuv91wzjB40Odx.e.2IUgMJi  ye aya encrypt password
+        })
+    })
 })
+// to check we use: bcrypt.compare("isme apna password","yahan encrypted string", function(err, result){
+                        //   console.log(result)     
+//                  })
+// abs scn ye hai ki ek barr login krne pe jab passowrd encrypt ho gya
+// phir humne phir login kra to password ko phirse encrypt krke database me to encrypt string h usko comapre h hai
+// mtlv encrpyt pass ko saved encypt password(string)se compare krte hai
+// encrypt krne ko hash bolte hai . so password ko hash banao or saved hash se compare karo
 
-app.get('/read', (req, res) => {
-    console.log(req.cookies)    // read krne k liye req use hota hai(yahan humne cokkie read kri)
-    res.send('read page')
-
-})
 
 app.listen(3000)
