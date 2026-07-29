c3784f7b06c6ef17d4181aaed3d6b68bfe536147
Kunal11102002 <bhardwajkunal0111@gmail.com>
2026-07-29 14:13:07 +0530

    commit

---
+++ 7.jwt_token/README.md
@@ -0,0 +1,16 @@
+hume krna hai create user account:
+       mongoose setup
+       schema
+       model
+       usercreate -> password -> hash
+       jwt token -> cookie
+
+
+
+       login Part k liye token recieve hoga use hum decrypt karenge 
+       usse hume email milega or hume pata lagega user kon hai..
+
+
+
+
+       YE H AJ KA KAM
--- 7.jwt_token/app.js
+++ 7.jwt_token/app.js
@@ -2,18 +2,32 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const cookieParser = require('cookie-parser')
+const userModel = require('./models/user')
 
 
 app.set('view engine', 'ejs')
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(cookieParser())
 
 
-app.get('/',function(req,res){
-    res.send('hello')
+app.get('/', function (req, res) {
+    res.render('index')
 
+})
+
+
+app.post('/create', async (req, res) => {
+    let { username, email, password, age } = req.body
+    let createdUser = await userModel.create({
+        username,
+        password,
+        email,
+        age
+
+    })
+    res.send(createdUser)
 })
 
 app.listen(3000)
