5e4d6d8ed605a2175b24c943a5a02f9192dad7f1
Kunal11102002 <bhardwajkunal0111@gmail.com>
2026-07-30 16:23:52 +0530

    commit

--- 8. data_association/app.js
+++ 8. data_association/app.js
@@ -18,5 +18,20 @@ app.get('/create', async function (req, res) {
     res.send(user)
     console.log(user)
 })
+app.get('/post/create', async function (req, res) {
+    let post = await postModel.create({
+        postdata: "hello sare log kaise ho",
+        user: "6a6b251b2724f2b4368e08ad"
+
+    })
+
+    let user = await userModel.findOne({ _id: '6a6b251b2724f2b4368e08ad' })
+    user.posts.push(post._id);
+    await user.save();
+    res.send({post , user});
+
+
+
+})
 
 app.listen(3000);
